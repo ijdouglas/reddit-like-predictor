efc1f02812ccac1f110906e98aa379bbb8749805
Ian Douglas <43050076+ijdouglas@users.noreply.github.com>
2022-11-18 09:34:16 -0600

    Update RedditScrape.py

--- RedditScrape.py
+++ RedditScrape.py
@@ -1,7 +1,9 @@
 """
-Define the class `RedditScrape` class that stores attributes to define a subreddit name and date range.
-Included methods will use these attributes to scrape either comments or posts in said sub for dates.
-Return pandas data frame of results and save to desired location.
+Define the `RedditScrape` class that stores attributes to define a subreddit name and date range.
+Included methods use these attributes to scrape either comments or posts in given subreddit within date range.
+The result of the above is to return a pandas data frame of results and save to desired location,
+using the method as_data_frame(self, 'comments') or as_data_frame(self, 'posts'). Other methods can save
+the data frame directly from the scraped results, and of course conduct scraping.
 
 This script does not create any instances, rather just defines the class itself!
 """
@@ -28,11 +30,9 @@ class RedditScrape:
     self.subreddit_name = subreddit_name
     self.start_epoch = start_epoch
     self.end_epoch = end_epoch
-  # To define another method (as in the __init__ method as well), the first
-  # argument is self so that within the method the attributes of the instance
-  # are accessible to the method
+
   
-  # Here's the method to scrape comments, specifically suing pushshift api
+  # Here's the method to scrape comments, specifically using pushshift api
   def scrape_comments(self):
     """
     Scrape comments, create self.comments attribute
@@ -44,7 +44,15 @@ class RedditScrape:
                                  after = self.start_epoch, 
                                  before = self.end_epoch)
     self.comments = [x for x in result] # extract into list, save as attribute
-
+    
+  def as_data_frame(self, attribute_name):
+    """
+    After comments or posts have been scraped using scrape_comments or scrape_posts,
+    they can be returned as a data frame
+    """
+    #import pandas as pd # already done in __init__
+    return(pd.DataFrame(getattr(self, attribute_name)))
+  
   # A method to save the desired data (comments, or later, posts)
   def save(self, attribute_name, filename):
     """
@@ -54,8 +62,3 @@ class RedditScrape:
     x = getattr(self, attribute_name)
     pd.DataFrame(x).to_csv(repr(filename), # basically r'filename'
                            encoding='utf-8-sig', index = False)
-# note that attributes can be edited AFTER inititialization directly
-# instance = RedditScrape('cat', dt.datetime(1111, 11, 11), dt.datetime(1112, 11, 11))
-# instance.subreddit_name = 'dog' # change subreddit name to dog
-# And this will not impact anything until scrape_comments() is run, after which point changes
-# to the subreddit_name attribute will mismatch comments attribute until scrape_comments is run again
